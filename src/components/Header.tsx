450bfe5c7275da259af5e8b94469f5fe8ef3236f
Seung Lee <leesh2985@naver.com>
2023-11-08 00:00:12 +0900

    # Refactor : header 부분

---
+++ src/components/Header.tsx
@@ -0,0 +1,64 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <LogoContainer>
+        <Logo src="..." alt="Logo" />
+      </LogoContainer>
+      <Nav>
+        <NavItem>
+          <NavLink to="/about">About me</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/skills">Skills</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/projects">Projects</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/career">Career</NavLink>
+        </NavItem>
+      </Nav>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: transparent;
+  padding: 5px 90px;
+  backdrop-filter: blur(6px);
+  box-shadow: none;
+  position: fixed;
+`;
+
+const LogoContainer = styled.div``;
+const Logo = styled.img``;
+
+const Nav = styled.nav`
+  display: flex;
+  padding: 10px 0;
+`;
+
+const NavItem = styled.li`
+  margin-right: 60px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: #242424;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: none;
+  }
+`;
