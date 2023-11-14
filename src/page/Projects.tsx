8364457ca2e0aafca147490f1f6c2011638c1764
Seung Lee <leesh2985@naver.com>
2023-11-15 00:05:54 +0900

    # Docs : Text 수정

--- src/page/Projects.tsx
+++ src/page/Projects.tsx
@@ -22,7 +22,7 @@ const projectsData: Project[] = [
     session: '23/09/05~23/11/10',
     type: '팀프로젝트(6인)',
     intro:
-      'Triptalk은 국내 여행의 추억과 경험을 공유하는 커뮤니티입니다. 여행 이야기를 듣고, 여행 목적지를 찾고, 더 나은 여행을 계획할 수 있는 다양한 정보를 제공합니다. 이 프로젝트는 React와 Vite를 기반으로 구축되었으며, Redux를 활용하여 상태를 효율적으로 관리합니다.',
+      'Triptalk은 국내 여행의 추억과 경험을 공유하는 커뮤니티로, 프론트엔드 3명과 백엔드 3명이 협업하여 빠른 개발 서버를 위해 Vite를 활용한 프로젝트입니다.  UI부분을 주도적으로 작업했으며 리뷰맵 내정보 개인정보수정페이지등을 작업을 맡았으며 반응형 디자인을 통해 다양한 디바이스에서의 사용을 고려하여 작업을 진행 했으며 이해가 어려운 부분이 발생하면 신속히 대화를 통해 의견을 공유해서 해결했습니다.',
     skills: [
       'https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=React&logoColor=black',
       'https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=Vite&logoColor=white',
@@ -39,7 +39,7 @@ const projectsData: Project[] = [
     vercelLink: 'https://hdrc.vercel.app/',
     session: '23/07/03~23/08/10',
     type: '개인프로젝트',
-    intro: `HERC 러닝 커뮤니티는 단체로 뛰기는 싫고 정보를 얻고 싶은 사람들을 위해 만들어진 커뮤니티입니다. 여기에서는 사용자들이 간편하게 러닝에 대한 정보를 얻고, 다른 러너들과 소통하며 소소한 모임을 즐길 수 있습니다.`,
+    intro: `HERC 러닝 커뮤니티는 단체로 뛰기를 꺼리는 러닝 열정을 가진 개인들을 위한 플랫폼입니다. 여기에서 러너들은 간편하게 러닝에 관한 정보를 얻고 서로 소통할 수 있는 커뮤니티를 형성합니다. 이 프로젝트는 Vite를 활용하여 빠른 개발 서버와 최적화를 위한 빌드 시간을 확보하였으며, 데이터베이스로는 파이어베이스를 효율적으로 활용하여 작업되었습니다.`,
     skills: [
       'https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=React&logoColor=black',
       'https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=Vite&logoColor=white',
@@ -58,7 +58,7 @@ const projectsData: Project[] = [
     session: '23/08/24~23/08/25',
     type: '개인프로젝트',
     intro:
-      'To-Do list 프로젝트는 수정 및 삭제 기능을 갖추고 있습니다. 더불어, 간편하게 오늘의 완료된 작업을 표시할 수 있는 기능도 제공합니다.',
+      'To-Do List 프로젝트는 수정 및 삭제 기능을 갖추고 있어 사용자가 편리하게 작업을 관리할 수 있습니다. 더불어, 간편한 완료 기능을 통해 오늘의 완료된 작업을 표시하는 기능을 제공합니다. 이 프로젝트는 HTML, CSS, JavaScript를 사용하여 구현되었으며, JS를 활용하여 명언이 새로 고침될 때마다 변경되게 했습니다.',
     skills: [
       'https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
       'https://img.shields.io/badge/css3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
@@ -72,7 +72,8 @@ const projectsData: Project[] = [
     vercelLink: 'https://lsh-one.vercel.app/',
     session: '23/11/13~23/11/20',
     type: '개인프로젝트',
-    intro: '한눈에 보기좋게 각 작업물을 모아서 만든 포트폴리오 사이트입니다.',
+    intro:
+      '이 프로젝트는 한눈에 보기 좋은 포트폴리오 웹사이트로, 각 작업물을 효과적으로 모아놓은 플랫폼입니다. Vite를 사용하여 개발되었으며, 가독성을 중점으로 하여 반응형 디자인을 구현하였습니다. ',
     skills: [
       'https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=React&logoColor=black',
       'https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=Vite&logoColor=white',
